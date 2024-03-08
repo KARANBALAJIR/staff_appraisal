7be77835e02aecf5d913ceefd881b6331df721a3
JayaSurya12345 <jayasurya.n2021csea@sece.ac.in>
2024-03-08 16:10:38 +0530

    Application 1 st page added...

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,16 +1,13 @@
 import SideNavbar from "./../../components/SideNavbar";
-export default async function Application() {
-    
-    
+import GeneralInfo from "./GeneralInfo";
 
-    
-    return (
-       
-        <div className="flex">
-            {/* <div className="ml-35 text-border">Have a nice day {user.firstName}.....</div> */}
-            <SideNavbar/>
-            <h1 className="text-center">Application</h1>     
-        </div>
-        
-    );
+export default async function Application() {
+  //
+  return (
+    <div className="flex">
+      {/* <div className="ml-35 text-border">Have a nice day {user.firstName}.....</div> */}
+      <SideNavbar />
+      <GeneralInfo />
+    </div>
+  );
 }
--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import React from "react";
+import Image from "next/image";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Disclosure } from "@headlessui/react";
 import {
@@ -19,39 +20,42 @@ function SideNavbar() {
   return (
     <div>
       <Disclosure as="nav">
-        
-        <div className="p-6 w-1/2 h-screen bg-blue-100 z-20 top-30 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+        <div className="p-6 w-full h-screen bg-blue-100 z-20 top-30 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
           <div className="flex flex-col justify-start item-center">
-           
             <div className=" my-8 border-b border-gray-100 pb-4">
-            <div className="flex  mb-8 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+              <div className="flex  mb-8 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <MdOutlineAnalytics className="text-2xl text-gray-600 group-hover:text-white " />
-                <Link href='/application'><h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  Application
-                </h3></Link>
-                
+                <Link href="/application">
+                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                    Application
+                  </h3>
+                </Link>
               </div>
               <div className="flex  mb-8 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <BiMessageSquareDots className="text-2xl text-gray-600 group-hover:text-white " />
-                <Link href='/announcements'>
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  Announcement
-                </h3>
+                <Link href="/announcements">
+                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                    Announcement
+                  </h3>
                 </Link>
-                
               </div>
               <div className="flex  mb-8 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <FaRegComments className="text-2xl text-gray-600 group-hover:text-white " />
-                <Link href='/feedback'><h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  Feedback
-                </h3></Link>
-                
+                <Link href="/feedback">
+                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                    Feedback
+                  </h3>
+                </Link>
               </div>
-              
-             
-              
             </div>
-           
+            <div>
+              <Image
+                src="/public/Eshwar-Logo.jpg"
+                alt="Image description"
+                width={300}
+                height={300}
+              />
+            </div>
           </div>
         </div>
       </Disclosure>
