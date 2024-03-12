be2be2bb40a35863eed4a5bf6a7167af4c257749
KARANBALAJIR <karanbalaji.r2021cseb@sece.ac.in>
2024-03-12 10:40:16 +0530

    Feedback form

--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -1,16 +1,42 @@
 import { auth, currentUser } from "@clerk/nextjs";
 import SideNavbar from "./../../components/SideNavbar";
 export default async function Feedback() {
-   
+    const Feedback = () => {
+        const handleSubmit = (e) => {  
+            e.preventDefault();
+            console.log("Feedback submitted");
+        }
+    }
 
     
     return (
         
         <div className="flex">
-            {/* <div className="ml-35 text-border">Have a nice day {user.firstName}.....</div> */}
             <SideNavbar/>
-            <h1 className="text-center">Feedback</h1>
+            <div className="flex-col ml-[400px]">
+                <h1 className="flex items-center justify-center mb[10] text-2xl">Feedback</h1>
+                <div className=" mt-36 w-[400px]">
+            
+                    <div className="mb-4 ">
+                    <label htmlFor="name" className="block text-black">Name:</label>
+                    <input type="text" id="name" name="name" required className="w-full px-3 py-2 border border-gray-700 rounded-md"></input>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-black">Email:</label>
+                    <input type="email" id="email" name="email" required className="w-full px-3 py-2 border border-gray-700 rounded-md"></input>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="message" className="block text-black">Message:</label>
+                    <textarea id="message" name="message" required className="w-full px-3 py-2 border border-gray-700 rounded-md"></textarea>
+                </div>
+
+                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Submit</button>
+                </div>
+            </div>
         </div>
-        
+       
     );
-}
+};
+
